504395c7984cd892befe00c067a33e8d28b676c3
jswashburn <77169258+jswashburn@users.noreply.github.com>
2021-04-07 12:29:47 -0400

    Add collectable

---
+++ RollerProj/Assets/Scripts/Core/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Roller.Environment.Items;
+using UnityEngine;
+
+namespace Roller.Core
+{
+    public class Player : MonoBehaviour
+    {
+        int _points = 0;
+        
+        void OnEnable()
+        {
+            Collectable.Collected += Collect;
+        }
+
+        void OnDisable()
+        {
+            Collectable.Collected -= Collect;
+        }
+
+        void Collect(Item item)
+        {
+            _points += (int) item;
+        }
+    }
+}
