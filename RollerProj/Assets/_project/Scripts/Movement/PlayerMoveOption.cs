bd53548570e99b78e6cf86ad6a78e72a28cb0389
jswashburn <77169258+jswashburn@users.noreply.github.com>
2021-04-07 10:09:38 -0400

    test commit rider IDE

--- RollerProj/Assets/_project/Scripts/Movement/PlayerMoveOption.cs
+++ RollerProj/Assets/_project/Scripts/Movement/PlayerMoveOption.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class PlayerMoveOption : IMoveOption
+public class PlayerMoveOption : IMoveOption // test comment for git
 {
     public Vector3 MoveDirection { get; set; }
     public bool JumpRequested { get; set; }
